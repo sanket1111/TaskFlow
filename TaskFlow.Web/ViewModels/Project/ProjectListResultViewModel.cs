e99a82f71c340e16b70ec54b4c45faa2c1712d30
Sanket Pabrekar <sanketpabrekar09@gmail.com>
2026-08-20 23:31:58 +0530

    Added ProjectListResultViewModel

---
+++ TaskFlow.Web/ViewModels/Project/ProjectListResultViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskFlow.Web.ViewModels.Project
+{
+    public class ProjectListResultViewModel
+    {
+        public List<ProjectListViewModel> Projects { get; set; } = new();
+        public int TotalCount { get; set; }
+    }
+}
