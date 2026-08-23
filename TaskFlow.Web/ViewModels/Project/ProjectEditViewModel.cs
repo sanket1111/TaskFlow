8f1ee2b080c63e87bbe8ce74bda3dc2197309eda
Sanket Pabrekar <sanket.pabrekar@sagitec.com>
2026-08-23 14:45:19 +0530

    LC

--- TaskFlow.Web/ViewModels/Project/ProjectEditViewModel.cs
+++ TaskFlow.Web/ViewModels/Project/ProjectEditViewModel.cs
@@ -1,15 +1,41 @@
-﻿using TaskFlow.Web.Models.Enum;
+﻿using System.ComponentModel.DataAnnotations;
+using TaskFlow.Web.Models.Enum;
+using TaskFlow.Web.Validation;
 
 namespace TaskFlow.Web.ViewModels.Project
 {
-    public class ProjectEditViewModel
+    public class ProjectEditViewModel : IValidatableObject
     {
         public int Id { get; set; }
+        [StringLength(50, ErrorMessage = "Project name cannot exceed 50 characters")]
+        [Required(ErrorMessage = "Project name is required")]
         public string ProjectName { get; set; } = string.Empty;
-        public string Description { get; set; } =string.Empty;
+        public string Description { get; set; } = string.Empty;
         public ProjectStatus Status { get; set; } = ProjectStatus.New;
         public ProjectPriority Priority { get; set; } = ProjectPriority.Medium;
-        public DateTime StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
+
+        [Required(ErrorMessage = "Start date is required")]
+        [DataType(DataType.Date)]
+        public DateTime? StartDate { get; set; } = null;
+
+        [DataType(DataType.Date)]
+        public DateTime? EndDate { get; set; } = null;
+        public string? LastModifiedDateField { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (StartDate.HasValue
+                && EndDate.HasValue
+                && EndDate.Value < StartDate)
+            {
+                var validationField = LastModifiedDateField == nameof(StartDate)
+                                        ? nameof(StartDate)
+                                        : nameof(EndDate);
+
+                yield return new ValidationResult(
+                    "End date cannot be earlier than start date",
+                    new[] { nameof(validationField) });
+            }
+        }
     }
 }
---
+++ TaskFlow.Web/wwwroot/js/project-edit.js
@@ -0,0 +1,7 @@
+﻿document.getElementById("StartDate").addEventListener("change", function () {
+    document.getElementById("LastModifiedDateField").value = "StartDate";
+});
+
+document.getElementById("EndDate").addEventListener("change", function () {
+    document.getElementById("LastModifiedDateField").value = "EndDate";
+});
