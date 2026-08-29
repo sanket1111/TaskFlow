bbe1aa9a8a9fa1e0f8870e462e86c577d2c83286
Sanket Pabrekar <sanketpabrekar09@gmail.com>
2026-08-29 14:23:40 +0530

    Delete functionality added.

---
+++ TaskFlow.Web/wwwroot/js/task-delete.js
@@ -0,0 +1,12 @@
+﻿const deleteTaskModal = document.getElementById("deleteTaskModal");
+
+deleteTaskModal.addEventListener("show.bs.modal", function (event) {
+
+    const button = event.relatedTarget;
+
+    const taskId = button.getAttribute("data-task-id");
+    const taskName = button.getAttribute("data-task-name");
+
+    document.getElementById("deleteTaskId").value = taskId;
+    document.getElementById("deleteTaskName").textContent = taskName;
+});
